4d7c845bf2ec62311e86a937f4ab4fcac523fa06
danystefanescu <danydavidgt@gmail.com>
2023-01-22 15:40:06 +0000

    Added features GET, POST AND GET-DELETE by ID

--- src/app.py
+++ src/app.py
@@ -8,8 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Character, Location, Episode
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,14 +35,260 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# <------------------- USER GET AND POST -------------------------------------------------------->
 
+@app.route('/user', methods=['POST','GET'])
+def handle_users():
+
+     if request.method == 'POST':
+        body = request.get_json()
+        user = User(
+            user_name=body['user_name'],
+            first_name=body['first_name'],
+            last_name=body['last_name'],
+            email= body['email'],
+            password=body['password'],
+            is_active=True         
+        )
+        db.session.add(user)
+        db.session.commit()
+        response_body = {
+        "msg": "User added correctly !"
+        }
+        return jsonify(response_body), 200
+
+     if request.method == 'GET':
+        users = User.query.all()
+        users_serialized = [x.serialize() for x in users]
+        response_body = {
+            "msg": "This is your /GET for table 'User' !"
+        }
+        return jsonify({"Users" : users_serialized}, response_body), 200
+
+
+# DELETE a USER based on it's ID ---------------------------------------------------------------------->
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user_by_id(user_id):
+    user_query = User.query.get(user_id)
+    if not user_query:
+        response_body = {
+            "msg" : "This user doesn't exist, can't be deleted."
+        }
+        return jsonify(response_body), 200
+
+    db.session.delete(user_query)
+    db.session.commit()
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg" : "User deleted correctly !"
     }
 
     return jsonify(response_body), 200
+
+# GET a USER based on it's ID ------------------------------------------------------------------------->
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user_by_id(user_id):
+    user_query = User.query.get(user_id)
+    
+    if not user_query:
+        response_body = {
+            "msg" : "The user you are looking for doesn't exist."
+        }
+        return jsonify(response_body), 200
+
+    user_serialize = user_query.serialize()
+    return jsonify({"Result": user_serialize}), 200
+
+
+# <------------------- CHARACTER GET AND POST -------------------------------------------------------->
+
+@app.route('/character', methods=['POST','GET'])
+def handle_characters():
+    if request.method == 'POST':
+        body = request.get_json()
+        character = Character(
+            name=body['name'],
+            species=body['species'],
+            gender=body['gender'],
+            is_alive=True   
+        )
+        db.session.add(character)
+        db.session.commit()
+        response_body = {
+        "msg": "Character added correctly !"
+        }
+        return jsonify(response_body), 200
+
+    if request.method == 'GET':
+        characters = Character.query.all()
+        characters_serialized = [x.serialize() for x in characters]
+        response_body = {
+            "msg": "This is your /GET for table 'Character' !"
+        }
+        return jsonify({"Characters" : characters_serialized}, response_body), 200
+
+
+# DELETE a CHARACTER based on it's ID ----------------------------------------------------------------->
+
+@app.route('/character/<int:character_id>', methods=['DELETE'])
+def delete_character_by_id(character_id):
+    character_query = Character.query.get(character_id)
+    if not character_query:
+        response_body = {
+            "msg" : "This character doesn't exist, can't be deleted."
+        }
+        return jsonify(response_body), 200
+
+    db.session.delete(character_query)
+    db.session.commit()
+    response_body = {
+        "msg" : "User deleted correctly !"
+    }
+
+    return jsonify(response_body), 200
+
+# GET a CHARACTER based on it's ID -------------------------------------------------------------------->
+
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_character_by_id(character_id):
+    character_query = Character.query.get(character_id)
+    
+    if not character_query:
+        response_body = {
+            "msg" : "The user you are looking for doesn't exist."
+        }
+        return jsonify(response_body), 200
+
+    character_serialize = character_query.serialize()
+    return jsonify({"Result": character_serialize}), 200
+
+
+# <------------------- LOCATION GET AND POST -------------------------------------------------------->
+
+@app.route('/location', methods=['POST','GET'])
+def handle_locations():
+    if request.method == 'POST':
+        body = request.get_json()
+        location = Location(
+            name=body['name'],
+            type=body['type'],
+            dimension=body['dimension']
+        )
+        db.session.add(location)
+        db.session.commit()
+        response_body = {
+        "msg": "Location added correctly !"
+        }
+        return jsonify(response_body), 200
+
+    if request.method == 'GET':
+        locations = Location.query.all()
+        locations_serialized = [x.serialize() for x in locations]
+        response_body = {
+            "msg": "This is your /GET for table 'Location' !"
+        }
+        return jsonify({"Locations" : locations_serialized}, response_body), 200
+
+
+# DELETE a LOCATION based on it's ID ---------------------------------------------------------------->
+
+@app.route('/location/<int:location_id>', methods=['DELETE'])
+def delete_location_by_id(location_id):
+    location_query = Location.query.get(location_id)
+    if not location_query:
+        response_body = {
+            "msg" : "This character doesn't exist, can't be deleted."
+        }
+        return jsonify(response_body), 200
+
+    db.session.delete(location_query)
+    db.session.commit()
+    response_body = {
+        "msg" : "User deleted correctly !"
+    }
+
+    return jsonify(response_body), 200
+
+# GET a CHARACTER based on it's ID -------------------------------------------------------------------->
+
+@app.route('/location/<int:location_id>', methods=['GET'])
+def get_location_by_id(location_id):
+    location_query = Location.query.get(location_id)
+    
+    if not location_query:
+        response_body = {
+            "msg" : "The user you are looking for doesn't exist."
+        }
+        return jsonify(response_body), 200
+
+    location_serialize = location_query.serialize()
+    return jsonify({"Result": location_serialize}), 200
+
+
+# <------------------- EPISODE GET AND POST -------------------------------------------------------->
+
+@app.route('/episode', methods=['POST','GET'])
+def handle_episodes():
+    if request.method == 'POST':
+        body = request.get_json()
+        episode = Episode(
+            name=body['name'],
+            air_date=body['air_date'],
+            episode=body['episode']
+        )
+        db.session.add(episode)
+        db.session.commit()
+        response_body = {
+        "msg": "Episode added correctly !"
+        }
+        return jsonify(response_body), 200
+
+    if request.method == 'GET':
+        episodes = Episode.query.all()
+        episodes_serialized = [x.serialize() for x in episodes]
+        response_body = {
+            "msg": "This is your /GET for table 'Episode' !"
+        }
+        return jsonify({"Episodes" : episodes_serialized}, response_body), 200
+
+
+# DELETE a EPISODE based on it's ID ---------------------------------------------------------------->
+
+@app.route('/episode/<int:episode_id>', methods=['DELETE'])
+def delete_episode_by_id(episode_id):
+    episode_query = Location.query.get(episode_id)
+    if not episode_query:
+        response_body = {
+            "msg" : "This character doesn't exist, can't be deleted."
+        }
+        return jsonify(response_body), 200
+
+    db.session.delete(episode_query)
+    db.session.commit()
+    response_body = {
+        "msg" : "User deleted correctly !"
+    }
+
+    return jsonify(response_body), 200
+
+# GET a EPISODE based on it's ID -------------------------------------------------------------------->
+
+@app.route('/episode/<int:episode_id>', methods=['GET'])
+def get_episode_by_id(episode_id):
+    episode_query = Episode.query.get(episode_id)
+    
+    if not episode_query:
+        response_body = {
+            "msg" : "The user you are looking for doesn't exist."
+        }
+        return jsonify(response_body), 200
+
+    episode_serialize = episode_query.serialize()
+    return jsonify({"Result": episode_serialize}), 200
+
+
+# <------------------- SERVER RUN ON PORT 3000 -------------------------------------------------------->
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,77 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# <------------------- USER -------------------------------------------------------->
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    user_name = db.Column(db.String(20), nullable=False, unique=True)
+    first_name = db.Column(db.String(25), nullable=False)
+    last_name = db.Column(db.String(25), nullable=False)
+    email = db.Column(db.String(25), nullable=False, unique=True)
+    password = db.Column(db.String(25), nullable=False, unique=True)
+    is_active = db.Column(db.Boolean, default=True)
 
     def serialize(self):
         return {
             "id": self.id,
+            "user_name": self.user_name,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             "email": self.email,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
+        }
+
+# <------------------- CHARACTER -------------------------------------------------------->
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False, unique=True)
+    species = db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
+    is_alive = db.Column(db.Boolean, default=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "species": self.species,
+            "gender": self.gender,
+            "is_alive": self.is_alive
+        }
+
+# <------------------- LOCATION -------------------------------------------------------->
+
+class Location(db.Model):
+    __tablename__ = 'location'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False, unique=True)
+    type = db.Column(db.String(250), nullable=False)
+    dimension = db.Column(db.String(250), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type": self.type,
+            "dimension": self.dimension
+        }
+
+# <------------------- EPISODE -------------------------------------------------------->
+
+class Episode(db.Model):
+    __tablename__ = 'episode'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False, unique=True)
+    air_date = db.Column(db.String(250), nullable=False)
+    episode = db.Column(db.String(250), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "air_date": self.air_date,
+            "episode": self.episode
         }
